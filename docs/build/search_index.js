723454b9f5bcf0b31ab75a87c7591820a0cd443f
LorenzoFendillo <lf234@njit.edu>
2024-01-09 16:42:32 +0100

    12

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Helloworld-Documentation","page":"Home","title":"Helloworld Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to the Helloworld documentation!","category":"page"},{"location":"page2/","page":"Page 2","title":"Page 2","text":"non lo so era per provare","category":"page"}]
+}
