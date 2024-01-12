95324322d29c6bcdf3da2f40c124fe7d6b9e7519
LorenzoFendillo <lf234@njit.edu>
2024-01-12 15:11:00 +0100

    pushFunzDoc

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ using Helloworld
 
 makedocs(
     sitename = "Test Documentation",
-    modules = [Helloworld],
+    modules = [Helloworld, Funzione],
     format = Documenter.HTML(),
     pages = [
         "Home" => "index.md",
