f563d6bdedc1591989405309708682e9cf4380c6
Umbertoneviani <un38@njit.edu>
2024-01-17 06:01:55 -0500

    latecProva

--- src/Esempio.jl
+++ src/Esempio.jl
@@ -6,6 +6,15 @@ export func
     func(x)
 
 Return double the number `x` plus `1`.
+
+e prova latec
+
+The Schrödinger equation in a docstring:
+
+``i\\hbar\\frac{\\partial}{\\partial t}\\Psi(r,t) = \\left[ \\frac{-\\hbar^2}{2m}\\nabla^2 + V(r,t) \\right] \\Psi(r,t)``
+
+This equation describes how the quantum state of a physical system changes over time.
+
 """
 func(x) = 2x + 1
 
